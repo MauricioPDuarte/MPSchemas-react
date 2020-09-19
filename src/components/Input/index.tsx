898c082853e66707dc1545d4eb871b969cb93e5a
Mauricio Duarte <prussdev@gmail.com>
2020-09-19 08:36:23 -0300

    Utilizando formik nos formularios

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,7 @@
 import React, { InputHTMLAttributes } from 'react';
+import { FiAlertCircle } from 'react-icons/fi';
 import { IconBaseProps } from 'react-icons';
+import { useField } from 'formik';
 
 import { Container } from './styles';
 
@@ -8,11 +10,16 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ComponentType<IconBaseProps>;
 }
 
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => (
-  <Container>
-    {Icon && <Icon size={20} />}
-    <input {...rest} type="text" />
-  </Container>
-);
+const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => {
+  const [field, meta] = useField(rest);
+
+  return (
+    <Container>
+      {Icon && <Icon size={20} />}
+      <input {...field} {...rest} type="text" />
+      {meta.touched && meta.error ? <FiAlertCircle size={20} /> : null}
+    </Container>
+  );
+};
 
 export default Input;
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,29 +1,62 @@
 import React from 'react';
 import { FiLock, FiMail, FiUser } from 'react-icons/fi';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content } from './styles';
 
-const SignUp: React.FC = () => (
-  <Container>
-    <Content>
-      <h1>Cadastrar-se</h1>
-      <form>
-        <Input name="name" placeholder="Nome" icon={FiUser} />
-        <Input name="email" placeholder="E-mail" icon={FiMail} />
-        <Input name="password" placeholder="Senha" icon={FiLock} />
-        <Input name="confirmPassword" placeholder="Confirmar senha" icon={FiLock} />
-        <Button>Cadastrar</Button>
-      </form>
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Content>
+        <h1>Cadastrar-se</h1>
 
-      <span>
-        Já possui uma conta?
-        <a href="/"> Fazer login</a>
-      </span>
-    </Content>
-  </Container>
-);
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            password: '',
+            passwordConfirm: '',
+          }}
+          validationSchema={Yup.object({
+            name: Yup.string()
+              .min(2, 'Deve conter no mínimo 2 caracteres')
+              .max(26, 'Deve conter no máximo 26 caracteres')
+              .required('Campo obrigatório'),
+            email: Yup.string().email('E-mail inválido').required('Campo obrigatório'),
+            password: Yup.string().required('Campo obrigatório'),
+            passwordConfirm: Yup.string()
+              .oneOf([Yup.ref('password')])
+              .required('Campo obrigatório'),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {(formik) => (
+            <form onSubmit={formik.handleSubmit}>
+              <Input name="name" placeholder="Nome" icon={FiUser} />
+              <Input name="email" type="email" placeholder="E-mail" icon={FiMail} />
+              <Input name="password" type="password" placeholder="Senha" icon={FiLock} />
+              <Input name="passwordConfirm" type="password" placeholder="Confirmar senha" icon={FiLock} />
+              <Button type="submit">Cadastrar</Button>
+            </form>
+          )}
+        </Formik>
+
+        <span>
+          Já possui uma conta?
+          <a href="/"> Fazer login</a>
+        </span>
+      </Content>
+    </Container>
+  );
+};
 
 export default SignUp;
